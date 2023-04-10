98c082de21ae5569e221a2132b373651aee9b1bf
HagiwaraMiona <124038780+HagiwaraMiona@users.noreply.github.com>
2023-04-10 12:51:23 +0800

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k;
+    printf("可能分配情况如下：\n");
+    for( i=0; i <= 100; i++ )
+        for( j=0; j <= 100; j++ )
+            for( k=0; k <= 100; k++ )
+            {
+                if( 5*i+3*j+k/5==100 && k%5==0 && i+j+k==100 )
+                {
+                    printf("公鸡 %2d 只，母鸡 %2d 只，小鸡 %2d 只\n", i, j, k);
+                }
+            }
+    return 0;
+}
